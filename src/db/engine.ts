092116f7c6a0c38a81e91b017c6a94056c274292
EXisTAnZ <ozim_ssspam@mail.ru>
2023-07-03 08:38:39 +0300

    feat: add db engine

---
+++ src/db/engine.ts
@@ -0,0 +1,56 @@
+import userDB from './db';
+import User from '../types/user';
+import { ERROR_MSG } from '../types/constants';
+
+export default class DBEngine {
+  private db: User[];
+
+  constructor() {
+    this.db = userDB;
+  }
+
+  public getUsers(): User[] {
+    return this.db;
+  }
+
+  public getUserById(id: string): User {
+    const user = this.existedUser(id);
+    if (!user) {
+      throw new Error(ERROR_MSG.USER_NOT_FOUND);
+    } else return user;
+  }
+
+  public addUser(user: User) {
+    if (this.isLoginUsed(user.username)) {
+      throw new Error(ERROR_MSG.LOGIN_USED);
+    } else this.db.push(user);
+  }
+
+  public updateUser(user: User) {
+    const userDB = this.existedUser(user.id);
+    if (!userDB) {
+      throw new Error(ERROR_MSG.USER_NOT_FOUND);
+    } else if (this.isLoginUsed(user.username)) {
+      throw new Error(ERROR_MSG.LOGIN_USED);
+    } else {
+      userDB.username = user.username;
+      userDB.age = user.age;
+      userDB.hobbies = user.hobbies;
+    }
+  }
+
+  public removeUser(id: string) {
+    if (!this.existedUser(id)) {
+      throw new Error(ERROR_MSG.USER_NOT_FOUND);
+    }
+    this.db.filter(user => user.id !== id);
+  }
+
+  private isLoginUsed(login: string) {
+    return !!this.db.find(user => user.username === login);
+  }
+
+  private existedUser(id?: string) {
+    return this.db.find(user => user.id === id);
+  }
+}
---
+++ src/types/constants.ts
@@ -0,0 +1,12 @@
+export enum HTTP_METHODS {
+  GET = 'GET',
+  POST = 'POST',
+  DELETE = 'DELETE',
+  PUT = 'PUT',
+}
+
+export enum ERROR_MSG {
+  LOGIN_USED = 'User with this login already registered',
+  INVALID_USER_ID = 'Invalid userID',
+  USER_NOT_FOUND = 'User not found',
+}
